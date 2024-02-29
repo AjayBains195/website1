438ee32a90512f343955285851415a80e8e3ad35
Ajay Bains <142863479+AjayBains195@users.noreply.github.com>
2024-02-29 14:03:28 -0800

    Expanding

---
+++ js/expanding.js
@@ -0,0 +1,16 @@
+panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListner('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+
+
+function removeActiveClasses(){
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
